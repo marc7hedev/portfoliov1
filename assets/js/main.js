3d0457c883c25f6f1340e3fb59ea627fd6fa43dc
marc7hedev <driberstagg@gmail.com>
2022-12-09 16:26:09 -0600

    Añadido selector de temas

--- assets/js/main.js
+++ assets/js/main.js
@@ -211,3 +211,19 @@ themeButton.addEventListener("click", () => {
     localStorage.setItem("selected-theme", getCurrentTheme());
     localStorage.setItem("selected-icon", getCurrentIcon());
 });
+
+//COLOR-SWITCHER
+document.querySelector(".switcher-btn").onclick = () => {
+    document.querySelector(".color-switcher").classList.toggle("active");
+}
+
+let themeButtons = document.querySelectorAll(".theme-buttons");
+
+themeButtons.forEach(color =>{
+
+    color.addEventListener("click", () =>{
+        let dataColor = color.getAttribute("data-color");
+        document.querySelector(":root").style.setProperty("--hue-color", dataColor);
+    })
+
+})
